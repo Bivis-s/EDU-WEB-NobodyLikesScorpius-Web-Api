d257e0b6207d0d952208733e2b3dba8fd4f2c566
Bivis-s <typoborov@yandex.by>
2021-04-17 14:32:26 +0300

    ArticleController.cs was added

--- TryToWebApi/Controllers/ArticleController.cs
+++ TryToWebApi/Controllers/ArticleController.cs
@@ -1,7 +1,21 @@
-﻿namespace TryToWebApi.Controllers
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TryToWebApi.objects;
+
+namespace TryToWebApi.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class ArticleController
     {
-        
+        [HttpGet]
+        public IEnumerable<Article> Get()
+        {
+            return new Article[]
+            {
+                new ("Учёные нашли лучший знак зодиака!", "Кто-то сказал, что водолеи лучше всех, мы им верим)", ZodiacType.Aquarius),
+                new ("Астрологи объявили неделю ебашилова", "Вас ждёт хорошая продуктивная следующая неделя", ZodiacType.Pisces)
+            };
+        }
     }
 }
--- TryToWebApi/objects/Article.cs
+++ TryToWebApi/objects/Article.cs
@@ -1,7 +1,23 @@
-﻿namespace TryToWebApi.objects
+﻿
+namespace TryToWebApi.objects
 {
     public class Article
     {
+        public string Title { get; set; }
         
+        public string Content { get; set; }
+        
+        public ZodiacType ZodiacType { get; set; }
+
+        public Article()
+        {
+        }
+
+        public Article(string title, string content, ZodiacType zodiacType)
+        {
+            Title = title;
+            Content = content;
+            ZodiacType = zodiacType;
+        }
     }
 }
