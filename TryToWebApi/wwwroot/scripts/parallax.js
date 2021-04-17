b4429b5d4bfa0a766b218e2b845bd3017358a6c9
Bivis-s <typoborov@yandex.by>
2021-04-17 15:02:10 +0300

    static site was added

---
+++ TryToWebApi/wwwroot/scripts/parallax.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+    this.querySelectorAll(".parallax-layer").forEach(layer => {
+        const speed = layer.getAttribute('data-speed');
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
